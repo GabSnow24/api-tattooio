ac2dc8742c2453622faceeb4c67e416f4bd34aa0
Gabriel Queiroz <gqueiroz_photo@hotmail.com>
2024-03-11 17:01:46 -0300

    feat: a lot of fixes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('user')
+@ApiTags('Users')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBody({ type: CreateUserDto })
+  @ApiOperation({ summary: 'Create customer' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The user has been created successfully.',
+    type: CreateUserDto,
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly prisma: PrismaService) { }
+  
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -18,6 +21,19 @@ export class UserService {
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
+  }
+
+  async findByUserName(username: string) {
+    const foundUser = await this.prisma.user.findFirst({
+      where: {
+        username
+      }
+    })
+    if (!foundUser) {
+      Logger.error('User not found', '', 'UserService', true)
+      throw new NotFoundException('User not found')
+    }
+    return foundUser
   }
 
   remove(id: number) {
